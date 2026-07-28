12a811c8b6822958419c082f172c6f1548d8d907
Dmitry Kotik <7944694+dkotik@users.noreply.github.com>
2026-07-28 21:23:09 +0300

    add inlined CSS plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,25 @@
 import { defineConfig } from 'astro/config';
-
 import playformCompress from "@playform/compress";
+
+import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [playformCompress({
-  		// Exclude: [
-			// 	"File.png",
-			// 	(File: string) =>
-			// 		File === "./Target/Favicon/Image/safari-pinned-tab.svg",
-			// ],
-			CSS: true,
-			HTML: {
-				"html-minifier-terser": {
-					removeAttributeQuotes: true,
-				},
-			},
-			Image: true,
-			JavaScript: true,
-			JSON: true,
-			SVG: true
-		})]
+    // Exclude: [
+    // 	"File.png",
+    // 	(File: string) =>
+    // 		File === "./Target/Favicon/Image/safari-pinned-tab.svg",
+    // ],
+    CSS: true,
+    HTML: {
+      "html-minifier-terser": {
+        removeAttributeQuotes: true
+      }
+    },
+    Image: true,
+    JavaScript: true,
+    JSON: true,
+    SVG: true
+  }), playformInline()]
 });
